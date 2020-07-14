c454ab1499e4a19ec8999af2b096b369036c32e3
MyPC@USERMIC-67CAIR7 <dungbacninh.2002@gmail.com>
2020-07-14 07:55:16 +0700

    asdfasdikfhsadf

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.connect,{
+mongoose.connect(decodeURIComponent(process.env.connect),{
     useNewUrlParser:true,useUnifiedTopology: true,useFindAndModify:false
 })
 mongoose.set('useCreateIndex', true);
